1f5728de2848c8089fc407bd2262632719bcbc94
DisposaBoy <disposaboy@dby.me>
2013-07-28 18:37:11 +0100

    * add general debounced signals

--- gosubl/ev.py
+++ gosubl/ev.py
@@ -1,4 +1,6 @@
+import sublime
 import threading
+import time
 import traceback
 
 class Event(object):
@@ -36,6 +38,45 @@ class Event(object):
 			self.lst.remove(f)
 
 		return self
+
+class Signal(Event):
+	def __init__(self, delay_ms):
+		Event.__init__(self)
+
+		self.n = 0
+
+		self.delay = max(10, delay_ms)
+		self.deadline = 0
+		self.running = False
+
+	def __call__(self):
+		with self.lck:
+			self.deadline = (self.tms() + self.delay)
+
+		self._sched()
+
+	def tms(self):
+		return int(time.time() * 1000)
+
+	def _sched(self, ms=0):
+		with self.lck:
+			if not self.running:
+				if ms <= 0:
+					ms = self.delay
+
+				self.running = True
+				sublime.set_timeout(self._timedout, ms)
+
+	def _timedout(self):
+		with self.lck:
+			self.running = False
+			deadline = self.deadline
+
+		ms = deadline - self.tms()
+		if ms <= 0:
+			Event.__call__(self)
+		else:
+			self._sched(ms)
 
 debug = Event()
 init = Event()
