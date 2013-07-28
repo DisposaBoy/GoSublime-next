993824e816489438475475e92857b99b37ca1236
DisposaBoy <disposaboy@dby.me>
2013-07-28 18:56:11 +0100

    * put and delete no longer takes a default arg * get now takes a factory function instead of a a value * getdef removed in favour of the factory function

--- gosubl/kv.py
+++ gosubl/kv.py
@@ -1,40 +1,40 @@
 import copy
 import threading
+import traceback
 
 class M(object):
 	def __init__():
 		self.lck = threading.Lock()
 		self.d = {}
 
-	def _get(k, d, set_default):
+	def _get(k, df):
 		# not passing d as default because the stored value can, itself, be `None`
 		v = self.d.get(k, None)
 
-		if v is None:
-			v = d
-
-			if set_default:
-				self.d[k] = copy.copy(v)
+		if v is None and df is not None:
+			try:
+				v, store = df()
+				if store:
+					self.d[k] = v
+			except Exception:
+				v = None
+				print(traceback.format_exc())
 
 		return v
 
-	def get(k, d=None):
+	def get(k, df=None):
 		with self.lck:
-			return copy.copy(self._get(k, d, False))
+			return copy.copy(self._get(k, df))
 
-	def getdef(k, d=None):
+	def put(k, v):
 		with self.lck:
-			return copy.copy(self._get(k, d, True))
-
-	def put(k, v, d=None):
-		with self.lck:
-			old_v = self._get(k, d, False)
+			old_v = self._get(k, None)
 			self.d[k] = v
 			return old_v
 
-	def delete(k, d=None):
+	def delete(k):
 		with self.lck:
-			old_v = self._get(k, d, False)
+			old_v = self._get(k, None)
 
 			try:
 				del self.d[k]
@@ -43,14 +43,17 @@ class M(object):
 
 			return old_v
 
+	def _df_zero(self):
+		return (0, False)
+
 	def incr(k, i=1):
 		with self.lck:
-			old_v = self._get(k, 0, False)
+			old_v = self._get(k, self._df_zero)
 			self.d[k] = old_v + i
 			return old_v
 
 	def decr(k, i=1):
 		with self.lck:
-			old_v = self._get(k, 0, False)
+			old_v = self._get(k, self._df_zero)
 			self.d[k] = old_v - i
 			return old_v
