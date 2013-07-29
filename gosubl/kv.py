e9037712f38de203863e61e51fa55fd658706354
DisposaBoy <disposaboy@dby.me>
2013-07-29 06:02:17 +0100

    * remove the ._get indirection as it's not useful anymore

--- gosubl/kv.py
+++ gosubl/kv.py
@@ -6,34 +6,31 @@ class M(object):
 		self.lck = threading.Lock()
 		self.d = {}
 
-	def _get(self, k, df):
-		# not passing d as default because the stored value can, itself, be `None`
-		v = self.d.get(k, None)
+	def get(self, k, df=None):
+		with self.lck:
+			v = self.d.get(k, None)
 
 		if v is None and df is not None:
 			try:
 				v, store = df()
 				if store:
-					self.d[k] = v
+					with self.lck:
+						self.d[k] = v
 			except Exception:
 				v = None
 				print(traceback.format_exc())
 
 		return v
 
-	def get(self, k, df=None):
-		with self.lck:
-			return self._get(k, df)
-
 	def put(self, k, v):
 		with self.lck:
-			old_v = self._get(k, None)
+			old_v = self.d.get(k, None)
 			self.d[k] = v
 			return old_v
 
 	def delete(self, k):
 		with self.lck:
-			old_v = self._get(k, None)
+			old_v = self.d.get(k, None)
 
 			try:
 				del self.d[k]
@@ -42,17 +39,14 @@ class M(object):
 
 			return old_v
 
-	def _df_zero(self):
-		return (0, False)
-
 	def incr(self, k, i=1):
 		with self.lck:
-			old_v = self._get(k, self._df_zero)
+			old_v = self.d.get(k, 0)
 			self.d[k] = old_v + i
 			return old_v
 
 	def decr(self, k, i=1):
 		with self.lck:
-			old_v = self._get(k, self._df_zero)
+			old_v = self.d.get(k, 0)
 			self.d[k] = old_v - i
 			return old_v
