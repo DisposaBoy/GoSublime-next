ff3971f7df0990ffcc1979a07e2a9761d2722cb2
DisposaBoy <disposaboy@dby.me>
2013-08-29 21:43:13 +0100

    * add a more proper impl of cancel-able commands... it looks a little iffy though

---
+++ src/gosubli.me/margo/proc.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"sync"
+	"time"
+)
+
+type ProcTable struct {
+	sync.Mutex
+
+	m map[string]*exec.Cmd
+}
+
+var procs = &ProcTable{
+	m: map[string]*exec.Cmd{},
+}
+
+func (p *ProcTable) Run(cid string, cmd *exec.Cmd) (error, time.Duration) {
+	if cid == "" {
+		cid = uid()
+	}
+
+	p.repl(cid, cmd)
+
+	start := time.Now()
+	err := cmd.Run()
+	dur := msDur(start)
+
+	p.Lock()
+	delete(p.m, cid)
+	p.Unlock()
+
+	return err, dur
+}
+
+func (p *ProcTable) Cancel(cid string) {
+	p.repl(cid, nil)
+}
+
+func (p *ProcTable) repl(cid string, cmd *exec.Cmd) {
+	c := p.set(cid, cmd)
+	if c != cmd {
+		for i := 0; i < 2; i++ {
+			c = p.set(cid, cmd)
+			if c == cmd {
+				break
+			}
+			c.Process.Signal(os.Interrupt)
+			p.sleep()
+		}
+
+		a := time.Now()
+
+		for c != cmd {
+			c.Process.Kill()
+			p.sleep()
+			c = p.set(cid, cmd)
+		}
+		postMessage("repl %s, %s", cid, msDur(a).String())
+	}
+}
+
+func (p *ProcTable) sleep() {
+	time.Sleep(50 * time.Millisecond)
+}
+
+func (p *ProcTable) set(cid string, cmd *exec.Cmd) *exec.Cmd {
+	p.Lock()
+	defer p.Unlock()
+
+	if c, ok := p.m[cid]; ok {
+		return c
+	}
+
+	if cmd != nil {
+		p.m[cid] = cmd
+	}
+
+	return cmd
+}
+
+func init() {
+	byeDefer(func() {
+		procs.Lock()
+		defer procs.Unlock()
+
+		for _, c := range procs.m {
+			c.Process.Kill()
+			c.Process.Release()
+		}
+	})
+}
