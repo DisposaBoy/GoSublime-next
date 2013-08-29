11e5222c838f5868bd8059290a3b7325ec14c9b0
DisposaBoy <disposaboy@dby.me>
2013-08-29 21:44:40 +0100

    * impl  cancel method on top of proc.*

---
+++ src/gosubli.me/margo/m_cancel.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+)
+
+type mCancel struct {
+	Cid string
+}
+
+func (m *mCancel) Call() (interface{}, string) {
+	start := time.Now()
+	procs.Cancel(m.Cid)
+	res := M{
+		m.Cid: true,
+		"dur": msDur(start).String(),
+	}
+	return res, ""
+}
+
+func init() {
+	registry.Register("cancel", func(b *Broker) Caller {
+		return &mCancel{}
+	})
+}
