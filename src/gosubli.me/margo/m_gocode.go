95dd5555adf671bb85929392d81ad7a96b345390
DisposaBoy <disposaboy@dby.me>
2013-12-15 15:07:52 +0000

    * add support for autocomplete InstallSuffix

--- src/gosubli.me/margo/m_gocode.go
+++ src/gosubli.me/margo/m_gocode.go
@@ -11,14 +11,15 @@ import (
 )
 
 type mGocode struct {
-	Autoinst bool
-	Env      map[string]string
-	Home     string
-	Dir      string
-	Builtins bool
-	Fn       string
-	Src      string
-	Pos      int
+	Autoinst      bool
+	InstallSuffix string
+	Env           map[string]string
+	Home          string
+	Dir           string
+	Builtins      bool
+	Fn            string
+	Src           string
+	Pos           int
 
 	calltip bool
 }
@@ -87,6 +88,7 @@ func (m *mGocode) Call() (interface{}, string) {
 
 func (g *mGocode) completions(src []byte, fn string, pos int) []gocode.MargoCandidate {
 	c := gocode.MargoConfig{}
+	c.InstallSuffix = g.InstallSuffix
 	c.Builtins = g.Builtins
 	c.GOROOT, c.GOPATHS = envRootList(g.Env)
 	return gocode.Margo.Complete(c, src, fn, pos)
