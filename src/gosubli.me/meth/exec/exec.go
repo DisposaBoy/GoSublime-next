c4d266faa9929ada8a2f042077fe1a1c01cb1c8d
DisposaBoy <disposaboy@dby.me>
2013-12-30 21:15:35 +0000

    * near copy-pasta from the nineo.py command design stage... i'm going to cross my finger and hope it correctly implements what is documented

--- src/gosubli.me/meth/exec/exec.go
+++ src/gosubli.me/meth/exec/exec.go
@@ -1,9 +1,11 @@
 package exec
 
 import (
+	"fmt"
 	"gosubli.me/mg"
 	"gosubli.me/sink"
 	"os/exec"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -12,6 +14,11 @@ import (
 var (
 	zChunk  = []byte{}
 	zChunks = [][]byte{}
+
+	caseCache = struct {
+		sync.Mutex
+		m map[string]*regexp.Regexp
+	}{m: map[string]*regexp.Regexp{}}
 )
 
 type StreamResp struct {
@@ -21,16 +28,22 @@ type StreamResp struct {
 }
 
 type Resp struct {
+	Attrs  []Attr
 	Chunks [][]byte
 	Ok     bool
 	Dur    string
 }
+
+type Attr map[string]string
 
 type Switch struct {
 	Case        []string
 	Negative    bool
 	Discard     bool
 	Fallthrough bool
+
+	cases []*regexp.Regexp
+	attrs []Attr
 }
 
 type Exec struct {
@@ -47,9 +60,10 @@ type Exec struct {
 	Switch        []Switch
 	SwitchOk      bool
 
-	brk  *mg.Broker
-	sink *sink.Chan
-	buf  struct {
+	switches []*Switch
+	brk      *mg.Broker
+	sink     *sink.Chan
+	buf      struct {
 		s     [][]byte
 		n     int
 		merge bool
@@ -57,6 +71,7 @@ type Exec struct {
 }
 
 func (e *Exec) Call() (interface{}, string) {
+	e.initSwitches()
 	c := exec.Command(e.Cmd, e.Args...)
 	c.Dir = e.Wd
 	c.Env = mg.Env(e.Env)
@@ -76,10 +91,18 @@ func (e *Exec) Call() (interface{}, string) {
 	err, dur := procs.Run(e.Cid, c)
 	e.sink.Close()
 	wg.Wait()
+
+	ok := c.ProcessState != nil && c.ProcessState.Success()
+	attrs, swOk := e.collectAttrs()
+	if e.SwitchOk && len(e.Switch) > 0 {
+		ok = swOk
+	}
+
 	res := Resp{
+		Attrs:  attrs,
 		Dur:    dur.String(),
 		Chunks: e.chunks(),
-		Ok:     c.ProcessState != nil && c.ProcessState.Success(),
+		Ok:     ok,
 	}
 
 	if err != nil {
@@ -96,6 +119,7 @@ func (e *Exec) stream(wg *sync.WaitGroup) {
 		select {
 		case s, ok := <-e.sink.C:
 			eof := !ok
+			s = e.sw(s)
 			e.put(s, eof)
 			if eof {
 				return
@@ -154,6 +178,103 @@ func (e *Exec) flush(eof bool) {
 	}
 	e.buf.s = e.buf.s[:0]
 	e.buf.n = 0
+}
+
+func (e *Exec) initSwitches() error {
+	for i, _ := range e.Switch {
+		sw := &e.Switch[i]
+		for _, s := range sw.Case {
+			if s != "" {
+				rx, err := rx(s)
+				if err != nil {
+					return err
+				}
+				sw.cases = append(sw.cases, rx)
+			}
+		}
+		e.switches = append(e.switches, sw)
+	}
+	return nil
+}
+
+func (e *Exec) collectAttrs() ([]Attr, bool) {
+	attrs := []Attr{}
+	ok := true
+	for _, sw := range e.switches {
+		if len(sw.attrs) > 0 {
+			ok = false
+			attrs = append(attrs, sw.attrs...)
+		}
+	}
+	return attrs, ok
+}
+
+func (e *Exec) sw(s []byte) []byte {
+	discard := false
+
+	for _, sw := range e.switches {
+		attr, matched := sw.match(s)
+		if len(attr) != 0 {
+			sw.attrs = append(sw.attrs, attr)
+		}
+
+		if matched != sw.Negative {
+			if sw.Discard {
+				discard = true
+			}
+
+			if !sw.Fallthrough {
+				break
+			}
+		}
+	}
+
+	if discard {
+		return nil
+	}
+	return s
+}
+
+func (s *Switch) match(p []byte) (Attr, bool) {
+	if len(s.cases) == 0 {
+		return nil, true
+	}
+
+	for _, rx := range s.cases {
+		mt := rx.FindSubmatch(p)
+		if len(mt) > 0 {
+			// p (and by extension mt) is owned by the caller so make sure it gets copied
+			names := rx.SubexpNames()
+			attr := Attr{}
+			if len(mt) == len(names) {
+				for i, k := range names {
+					if k != "" {
+						attr[k] = string(mt[i])
+					}
+				}
+			}
+			return attr, true
+		}
+	}
+
+	return nil, false
+}
+
+func rx(s string) (*regexp.Regexp, error) {
+	caseCache.Lock()
+	defer caseCache.Unlock()
+
+	if rx, ok := caseCache.m[s]; ok {
+		return rx, nil
+	}
+
+	rx, err := regexp.Compile(s)
+	if err != nil {
+		return nil, fmt.Errorf("cannot compile regexp `%v`: %v", s, err.Error())
+	}
+
+	caseCache.m[s] = rx
+	return rx, nil
 }
 
 func crSuffix(s []byte) bool {
